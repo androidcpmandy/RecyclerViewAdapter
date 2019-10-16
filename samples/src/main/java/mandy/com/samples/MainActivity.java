5b9a97a2b5c275a80ecde666448f910d3f5a457b
chengpan <642894580@qq.com>
2019-10-16 18:14:06 +0800

    samples demo代码上传

---
+++ samples/src/main/java/mandy/com/samples/MainActivity.java
@@ -0,0 +1,13 @@
+package mandy.com.samples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
